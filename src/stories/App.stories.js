33c4c34c1ddbfcb2a66547871734410fc8eac1eb
madhuri <madhurisravan.chitikela@gmail.com>
2023-05-05 19:13:47 +0530

    update name of component

--- src/stories/App.stories.js
+++ src/stories/App.stories.js
@@ -1,4 +1,4 @@
-import  App  from "./components/App";
+import  App  from "../stories/components/App";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
