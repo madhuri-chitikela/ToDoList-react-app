e376b7145fff33b8408470be7580dcad1731abe1
madhuri <madhurisravan.chitikela@gmail.com>
2023-05-05 19:45:14 +0530

    update the path

--- src/stories/App.stories.js
+++ src/stories/App.stories.js
@@ -1,4 +1,4 @@
-import  App  from "../stories/components/App";
+import  App  from "../components/App";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
--- src/stories/List.stories.js
+++ src/stories/List.stories.js
@@ -1,4 +1,4 @@
-import List from '../stories/components/List';
+import List from '../components/List';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
